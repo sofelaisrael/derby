06fe76bb8d7f7687bf561955130468cdb34bde85
sofelaisrael <sofelaisrael@users.noreply.github.com>
2026-09-22 18:01:09 +0100

    Bump compileSdk to 36 for dependency compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "uk.co.derbybins.derby_bins"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
