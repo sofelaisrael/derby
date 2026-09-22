74e6ffe91a5f0f590c61607ef3b114333f922468
sofelaisrael <sofelaisrael@users.noreply.github.com>
2026-09-22 18:42:54 +0100

    Match Mansfield clean build.gradle.kts - remove compileSdk override hack

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,16 +9,14 @@ val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+rootProject.layout.buildDirectory = newBuildDir
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-    project.plugins.withId("com.android.library") {
-        project.extensions.configure<com.android.build.gradle.LibraryExtension>("android") {
-            compileSdkVersion = "36"
-        }
-    }
+    project.layout.buildDirectory = newSubprojectBuildDir
+}
+subprojects {
+    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
